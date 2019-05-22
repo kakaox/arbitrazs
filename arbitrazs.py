172e044ed0f072a03872e4d048dcb4ef4b95716c
kakaox <kakaox@gmail.com>
2019-05-22 11:08:40 +0200

    bamm

--- arbitrazs.py
+++ arbitrazs.py
@@ -6,47 +6,62 @@ import numpy as np
 
 
 URL = "wss://ws-feed.gdax.com"
+outfilename = "arbitrazs.txt"
+outfile = open(outfilename,"a+")
 
 class CryptoData:
 	name = ""
 	asks = []
 	bids = []
+	last_update = ""
 	def __init__(self, name):
 		self.name = name
 
 	def parseData(self, msg):
-		print("%s - %s" % (self.name,msg))
 		if msg["product_id"]==self.name:
+			#print("%s - %s" % (self.name,msg))
 			if msg["type"]=="snapshot":
 				self.parseSnapshot(msg)
-			else:
-				if msg["type"]=="l2update":
-					self.parseUpdate(msg)
+			if msg["type"]=="l2update":
+				self.parseUpdate(msg)
 			self.printBook()
 
 	def parseSnapshot(self, msg):
-		asks=[(float(msg["asks"][i][0]), float(msg['asks'][i][1])) for i in range(10)]
-		bids = [(float(msg["bids"][i][0]), float(msg['bids'][i][1])) for i in range(10)]
+		self.asks=[(float(msg["asks"][i][0]), float(msg['asks'][i][1])) for i in range(len(msg['asks']))]
+		self.bids = [(float(msg["bids"][i][0]), float(msg['bids'][i][1])) for i in range(len(msg['bids']))]
 
 	def parseUpdate(self, msg):
+		#print(msg)
+		self.last_update=msg["time"]
 		for value in msg["changes"]:
 			if (float(value[2]) == 0):
 				priceToRemove = float(value[1])
-				if value[0]=="buy":
+				if value[0]=="sell":
+					#print("before delete %s (%s)" % (len(self.asks), priceToRemove))
 					self.asks = list(filter(lambda x : x[0] != priceToRemove, self.asks))
+					#print("after delete %s" % (len(self.asks)))
 				else:
 					self.bids = list(filter(lambda x : x[0] != priceToRemove, self.bids))
 			else:
-				if value[0]=="buy":
+				if value[0]=="sell":
 					self.asks.append((float(value[1]), float(value[2])))
 				else:
 					self.bids.append((float(value[1]), float(value[2])))
 
 	def printBook(self):
-		for i in range(len(asks)):
-			print("%d %f %f   ---   %f %f" % (i,asks[i][0],asks[i][1],bids[i][0],bids[i][0]))
+		self.asks=sorted(self.asks, key = lambda x: x[0]);
+		self.bids=sorted(self.bids, key = lambda x: x[0], reverse = True);
+		outfile.write("%s orderbook at %s was: \n" % (self.name,self.last_update))
+		for i in range(min(len(self.asks),10)):
+			outfile.write("%d \t %f %f \t --- \t %f %f \n" % (i,self.bids[i][0],self.bids[i][1],self.asks[i][0],self.asks[i][1]))
+
+	def printBestLevel(self):
+		outfile.write("%s best bid: %f, best ask: %f \n" % (self.name, self.bids[0][0], self.asks[0][0]))
+
 
 r_BTCUSD = CryptoData("BTC-USD")
+r_ETHUSD = CryptoData("ETH-USD")
+r_ETHBTC = CryptoData("ETH-BTC")
 
 def on_message(_, message):
 	"""Callback executed when a message comes.
@@ -57,162 +72,19 @@ def on_message(_, message):
 	global time_ask_BTCUSD, time_bid_BTCUSD, time_ask_ETHUSD, time_bid_ETHUSD, time_ask_ETHBTC, time_bid_ETHBTC, time_all
 	a = loads(message)
 	r_BTCUSD.parseData(a)
-    #print(a)
-    #if a['type']=='snapshot':
-        #i=0
-        #print('Snapshot arrived.')"""
-
-    #if a['type']=='l2update':
-        #i=i+1
-        #print('Update arrived.', a['changes'][0][0], a['product_id'])
-        #print(a)"""
-
-	#if a["type"]=="snapshot" and (a["product_id"]=="BTC-USD"):
-       #time_ask_BTCUSD, time_bid_BTCUSD, time_ask_ETHUSD, time_bid_ETHUSD, time_ask_ETHBTC, time_bid_ETHBTC, time_all = [], [], [], [], [], [], []
-       #r_ask_BTCUSD=[(float(a["asks"][i][0]), float(a['asks'][i][1])) for i in range(10)]
-       #r_bid_BTCUSD = [(float(a["bids"][i][0]), float(a['bids'][i][1])) for i in range(10)]
-
-    #if a["type"] == "snapshot" and (a["product_id"] == "ETH-BTC"):
-        #r_ask_ETHBTC = [(float(a["asks"][i][0]), float(a['asks'][i][1])) for i in range(10)]
-        #r_bid_ETHBTC = [(float(a["bids"][i][0]), float(a['bids'][i][1])) for i in range(10)]
-
-    #if a["type"] == "snapshot" and (a["product_id"] == "ETH-USD"):
-        #r_ask_ETHUSD = [(float(a["asks"][i][0]), float(a['asks'][i][1])) for i in range(10)]
-        #r_bid_ETHUSD = [(float(a["bids"][i][0]), float(a['bids'][i][1])) for i in range(10)]
-
-    #print('Init : ', r_ask_BTCUSD)
-
-    #-------------ORDERBOOK KIVONATOK------------------------------------------------------------
-    #--------------------------------------------------------------------------------------------
-
-    #--------------------------------------------------------------------------------------------
-    #-------------BTC-USD frissítések------------------------------------------------------------
-
-	if a["type"] == "l2update" and (a["product_id"] == "BTC-USD") and a["changes"][0][0] == "buy":
-        # print('SHIT HAPPENS!')
-		dtype = [('price', float), ('size', float)]
-		for value in a['changes']:
-            # print(value)
-            # print(len(r_ask_BTCUSD))
-			if (float(value[2]) == 0):
-				j = float(value[1])
-                # print(j)
-				for k in range(len(r_ask_BTCUSD)):
-					if (r_ask_BTCUSD[k][0] == j):
-                        # print(r_ask_BTCUSD[k][0])
-						r_ask_BTCUSD = np.delete(r_ask_BTCUSD, k)
-
-			else:
-				r_ask_BTCUSD.append((float(value[1]), float(value[2])))
-				time_ask_BTCUSD.append(a["time"])
-		_r_ask_BTCUSD = np.array(r_ask_BTCUSD, dtype=dtype)
-		_r_ask_BTCUSD = np.sort(_r_ask_BTCUSD, order='price')[::1]
-		r_ask_BTCUSD = [_r_ask_BTCUSD[i] for i in range(min(len(_r_ask_BTCUSD), 10))]
-        # print("ask")
-        # print(r_ask_BTCUSD)
-        # print(value[2])
-        # print(len(time_ask_BTCUSD))
-        # print(time_ask_BTCUSD[0])
-        # print(r_ask_BTCUSD)
-        # print(_r_ask_BTCUSD)
-        # print('On update 1 : ', r_ask_BTCUSD[0])
-        # exit(1)
-
-
-'''	if a["type"]=="l2update" and (a["product_id"]=="BTC-USD") and a["changes"][0][0]=="sell":
-		dtype = [('price', float), ('size', float)]
-        for value in a['changes']:
-            r_bid_BTCUSD.append((float(value[1]), float(value[2])))
-            time_bid_BTCUSD.append(a["time"])
-        _r_bid_BTCUSD = np.array(r_bid_BTCUSD, dtype=dtype)
-        _r_bid_BTCUSD = np.sort(_r_bid_BTCUSD, order='price')[::-1]
-        r_bid_BTCUSD = [_r_bid_BTCUSD[i] for i in range(10)]
-        #print("bid")
-        #print(r_bid_BTCUSD)
-        #print(time_bid_BTCUSD[0])
-        #print(r_bid_BTCUSD)
-        #print(_r_bid_BTCUSD)
-        #print('On update 2 : ', r_bid_BTCUSD[0])
-        #exit(1)
-
-    #--------------------------------------------------------------------------------------------
-    #-------------ETH-USD frissítések------------------------------------------------------------
-
-	if a["type"]=="l2update" and (a["product_id"]=="ETH-USD") and a["changes"][0][0]=="buy":
-        #print('SHIT HAPPENS!')
-		dtype = [('price', float), ('size', float)]
-        for value in a['changes']:
-            #print(value)
-            r_ask_ETHUSD.append((float(value[1]), float(value[2])))
-            time_ask_ETHUSD.append(a["time"])
-        _r_ask_ETHUSD = np.array(r_ask_ETHUSD, dtype=dtype)
-        _r_ask_ETHUSD = np.sort(_r_ask_ETHUSD, order='price')[::1]
-        r_ask_ETHUSD = [_r_ask_ETHUSD[i] for i in range(10)]
-        #print(time_ask_ETHUSD[0])
-        """print(r_ask_ETHUSD)
-        print(_r_ask_ETHUSD)
-        print('On update 2 : ', r_ask_ETHUSD[0])
-        #exit(1)"""
-
-
-	if a["type"]=="l2update" and (a["product_id"]=="ETH-USD") and a["changes"][0][0]=="sell":
-        #print('SHIT HAPPENS!')
-		dtype = [('price', float), ('size', float)]
-        for value in a['changes']:
-            #print(value)
-            r_bid_ETHUSD.append((float(value[1]), float(value[2])))
-            time_bid_ETHUSD.append(a["time"])
-        _r_bid_ETHUSD = np.array(r_bid_ETHUSD, dtype=dtype)
-        _r_bid_ETHUSD = np.sort(_r_bid_ETHUSD, order='price')[::-1]
-        r_bid_ETHUSD = [_r_bid_ETHUSD[i] for i in range(10)]
-        #print(time_bid_ETHUSD[0])
-        #print(r_bid_ETHUSD)
-        #print(_r_ask_ETHUSD)
-        #print('On update 2 : ', r_ask_ETHUSD[0])
-        #exit(1)
-
-    #--------------------------------------------------------------------------------------------
-    #-------------ETH-BTC frissítések------------------------------------------------------------
-
-	if a["type"]=="l2update" and (a["product_id"]=="ETH-BTC") and a["changes"][0][0]=="buy":
-		dtype = [('price', float), ('size', float)]
-        for value in a['changes']:
-            if (float(value[2])!=0):
-                r_ask_ETHBTC.append((float(value[1]), float(value[2])))
-                time_ask_ETHBTC.append(a["time"])
-        _r_ask_ETHBTC = np.array(r_ask_ETHBTC, dtype=dtype)
-        _r_ask_ETHBTC = np.sort(_r_ask_ETHBTC, order='price')[::1]
-        r_ask_ETHBTC = [_r_ask_ETHBTC[i] for i in range(10)]
-        #print(time_ask_ETHBTC[0])
-        #print(r_ask_ETHBTC)
-        #print(_r_ask_ETHBTC)
-        #print('On update 2 : ', r_ask_ETHBTC[0])
-        #exit(1)"""
-
-
-
-	if a["type"]=="l2update" and (a["product_id"]=="ETH-BTC") and a["changes"][0][0]=="sell":
-        #print('SHIT HAPPENS!')
-		dtype = [('price', float), ('size', float)]
-        for value in a['changes']:
-            #print(value)
-            r_bid_ETHBTC.append((float(value[1]), float(value[2])))
-            time_bid_ETHBTC.append(a["time"])
-        _r_bid_ETHBTC = np.array(r_bid_ETHBTC, dtype=dtype)
-        _r_bid_ETHBTC = np.sort(_r_bid_ETHBTC, order='price')[::-1]
-        r_bid_ETHBTC = [_r_bid_ETHBTC[i] for i in range(10)]
-
-        #print(r_bid_ETHBTC)
-        #print(_r_ask_ETHBTC)
-        #print('On update 2 : ', r_ask_ETHBTC[0])
-        #exit(1)'''
+	r_ETHUSD.parseData(a)
+	r_ETHBTC.parseData(a)
 
     #--------------------------------------------------------------------------------------------
     #-------------MŰVELETEK AZ ORDERBOOKKAL------------------------------------------------------------
 
-	#arbitrazs_1=1-((r_ask_ETHBTC[0][0]*r_bid_BTCUSD[0][0])/r_bid_ETHUSD[0][0])
-	#arbitrazs_2=1-(r_ask_ETHUSD[0][0]/(r_bid_ETHBTC[0][0]*r_bid_BTCUSD[0][0]))
-    #print("%f \t %f \t %f \t %f \t %f \t %f \t %f \t %f" % (r_ask_BTCUSD[0][0], r_bid_BTCUSD[0][0], r_ask_ETHUSD[0][0], r_bid_ETHUSD[0][0], r_ask_ETHBTC[0][0], r_bid_ETHBTC[0][0], arbitrazs_1, arbitrazs_2))
+	arbitrazs_1=1-((r_ETHBTC.bids[0][0]*r_BTCUSD.bids[0][0])/r_ETHUSD.bids[0][0])
+	arbitrazs_2=1-(r_ETHUSD.asks[0][0]/(r_ETHBTC.bids[0][0]*r_BTCUSD.bids[0][0]))
+	r_BTCUSD.printBestLevel()
+	r_ETHUSD.printBestLevel()
+	r_ETHBTC.printBestLevel()
+	outfile.write("arbitrazs_1: %f\n" % (arbitrazs_1))
+	outfile.write("arbitrazs_2: %f\n" % (arbitrazs_2))
 
 
 
@@ -239,8 +111,10 @@ def on_open(socket):
     socket.send(dumps(params))
 
 def main():
-    ws = WebSocketApp(URL, on_open=on_open, on_message=on_message)
-    ws.run_forever()
+	print("Getting orderbook from %s....\noutput is writing to %s" % (URL,outfilename))
+	ws = WebSocketApp(URL, on_open=on_open, on_message=on_message)
+	ws.run_forever()
+	outfile.close()
 
 
 if __name__ == '__main__':
